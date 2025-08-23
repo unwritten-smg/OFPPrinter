065ac6316b3ee609509825a5f037524e5a300ede
Nils Krautkraemer <krautkraemer.nils@outlook.com>
2025-08-23 17:00:35 +0200

    Change printing functions

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from dotenv import load_dotenv
 import os
 import src.download_ofp as gsb
 import src.print_data as stp
+import src.check_data_exists as check_data
 
 #Loading env file
 load_dotenv()
 
 #Variables
-user_id = os.getenv("SIMBRIEF_ID")
-file_path = "data\placeholder.txt"
-
+user_id = os.getenv("simbrief_id")
+file_path = os.getenv("file_path")
 
 
 def main():
+    #Check if folder data exists
+    check_data
     #Download Simbrief OFP
     gsb.downlaod_ofp(user_id)
     
---
+++ src/check_data_exists.py
@@ -0,0 +1,10 @@
+# Imports 
+import os
+
+data_path = "./data"
+
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+    
+else:
+    None
